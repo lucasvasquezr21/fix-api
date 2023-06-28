fdc35899eb51b012756257fd2ef7cf429f1dd7df
lucasvasquezr <lucas.vasquez@audibots.com>
2023-06-27 22:45:41 -0400

    cambios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // src/index.js
 import app from "./app.js";
 import connectDB from "./db.js";
+import dotenv from 'dotenv';
 
 // Conectarse a la base de datos de MongoDB
 connectDB();
 
+dotenv.config();
 // Iniciar el servidor
-app.listen(3000, () => {
-  console.log("Servidor iniciado en el puerto 3000");
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {console.log("Servidor iniciado en el puerto 3000");});
