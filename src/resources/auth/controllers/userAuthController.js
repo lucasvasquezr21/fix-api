9ba9bc535ee9b0b5e887feb7de653129fbe4e2d6
lavr21 <83773725+lucasvasquezr21@users.noreply.github.com>
2023-10-06 20:50:24 -0300

    Change: Se realiza cambio en token de inicio y registro de sesión

--- src/resources/auth/controllers/userAuthController.js
+++ src/resources/auth/controllers/userAuthController.js
@@ -5,7 +5,7 @@ import User from '../../models/users/userModel.js';
 
 export const registerUser = async (req, res) => {
   try {
-    const { name, email, rut, password, country, city, phone,  rol, comuna, data} = req.body;
+    const { name, email, rut, password, country, city, phone, rol, comuna, data } = req.body;
 
     // Verificar si ya existe un usuario con el mismo correo electrónico
     const existingUser = await User.findOne({ email });
@@ -15,11 +15,11 @@ export const registerUser = async (req, res) => {
 
     // Crear un nuevo usuario
     const hashedPassword = await bcryptjs.hash(password, 10);
-    const newUser = new User({ name, email, password: hashedPassword, rut, country, city, phone, rol, comuna, data});
+    const newUser = new User({ name, email, password: hashedPassword, rut, country, city, phone, rol, comuna, data });
     await newUser.save();
 
-    // Generar un token de acceso
-    const accessToken = jwt.sign({ userId: newUser._id }, environment.secretKey, { expiresIn: '50m' });
+    // Generar un token de acceso sin fecha de expiración
+    const accessToken = jwt.sign({ userId: newUser._id }, environment.secretKey);
 
     // Enviar una respuesta al cliente
     res.status(201).json({ success: true, accessToken });
@@ -41,16 +41,16 @@ export const loginUser = async (req, res) => {
 
     const isPasswordValid = await bcryptjs.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({success: false,  message: 'Credenciales inválidas' });
+      return res.status(401).json({ success: false, message: 'Credenciales inválidas' });
     }
 
-    // Generar un token de acceso
-    const accessToken = jwt.sign({ userId: user._id }, environment.secretKey, { expiresIn: '50m' });
+    // Generar un token de acceso sin fecha de expiración
+    const accessToken = jwt.sign({ userId: user._id }, environment.secretKey);
 
     // Enviar una respuesta al cliente
     res.status(200).json({ accessToken });
   } catch (error) {
     console.error(error);
-    res.status(500).json({success: false, message: 'Ha ocurrido un error al iniciar sesión' });
+    res.status(500).json({ success: false, message: 'Ha ocurrido un error al iniciar sesión' });
   }
 };
